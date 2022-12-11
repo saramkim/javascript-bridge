af5e331bd0623290379e09377a332d5bdef0f376
saram <yujun1018@naver.com>
2022-12-11 17:14:14 +0900

    feat: 다리 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const BridgeGame = require('./BridgeGame');
+const BridgeMaker = require('./BridgeMaker');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+
 class App {
+  #bridgeGame;
+
   play() {}
+
+  make(size) {
+    const bridge = BridgeMaker.makeBridge(
+      size,
+      BridgeRandomNumberGenerator.generate
+    );
+    this.#bridgeGame = new BridgeGame(bridge);
+  }
 }
 
 module.exports = App;
